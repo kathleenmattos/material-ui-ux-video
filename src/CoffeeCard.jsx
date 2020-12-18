8f680ca03b1b382094f87308a22b21516f60b7a6
Kathleen Mattos <kmattos1@cnm.edu>
2020-12-17 22:17:20 -0700

    building out cards

--- src/CoffeeCard.jsx
+++ src/CoffeeCard.jsx
@@ -1,55 +1,69 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import Card from '@material-ui/core/Card';
+import Avatar from '@material-ui/core/Avatar';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import ShareIcon from '@material-ui/icons/Share';
+import IconButton from '@material-ui/core/IconButton';
+import { CardMedia } from '@material-ui/core';
+
 
 
 const useStyles = makeStyles({
     bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
     },
     title: {
-      fontSize: 14,
+        fontSize: 14,
     },
     pos: {
-      marginBottom: 12,
+        marginBottom: 12,
     },
-  });
+    cardImageHeightStyles: {
+        height: 150,
+    }
+});
 
 
-const CoffeeCard = () => {
+const CoffeeCard = (props) => {
     const classes = useStyles();
-    const bull = <span className={classes.bullet}>•</span>;
-    return ( 
+    // const bull = <span className={classes.bullet}>•</span>;
+    const { avatarSrc, title, subtitle, description, imgSrc } = props;
+    return (
 
         <Card>
-        <CardContent>
-          <Typography className={classes.title} color="textSecondary" gutterBottom>
-            Word of the Day
-          </Typography>
-          <Typography variant="h5" component="h2">
-            be{bull}nev{bull}o{bull}lent
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            adjective
-          </Typography>
-          <Typography variant="body2" component="p">
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
+            <CardHeader
+                avatar={<Avatar src={avatarSrc} />}
+                action={
+                    <IconButton aria-label="settings">
+                        <ShareIcon />
+                    </IconButton>
+                }
+                title={title}
+                subheader={subtitle}
+            />
+            <CardMedia
+                className={classes.cardImageHeightStyles}
+                image={imgSrc}
+            />
+            <CardContent>
+                <Typography variant="body2" component="p">
+                    {description}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Buy Now!</Button>
+                <Button size="small">Offer</Button>
+            </CardActions>
+        </Card>
 
-     );
+    );
 }
- 
+
 export default CoffeeCard;
